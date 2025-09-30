6ac2cc354ee2c5ec1d4e0d68cf3e0041a361cb96
Sohan-De <Sohank.design@yahoo.com>
2025-09-30 23:21:10 +0600

    RTERTRET

--- scripts/script.js
+++ scripts/script.js
@@ -638,7 +638,7 @@ function initializeLoader() {
                 });
         }
         
-        loaderImage.alt = 'CrownPhone Logo';
+        loaderImage.alt = '';
         loaderImage.style.cssText = `
             width: 120px;
             height: 120px;
@@ -690,6 +690,12 @@ function initializeLoader() {
                 @keyframes rotateCircle {
                     0% { transform: rotate(0deg); }
                     100% { transform: rotate(360deg); }
+                }
+                
+                .loader-logo img {
+                    text-indent: -9999px;
+                    font-size: 0;
+                    line-height: 0;
                 }
             </style>
         `;
